ce4280c86271b7aec3d5ded1832352b4e09fe9b6
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2023-05-08 22:46:32 -0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const ano_input = document.getElementById('iAno')
-const masculino = document.getElementById('iop_masc')
-const feminino = document.getElementById('iop_femin')
+let ano_input = document.getElementById('iAno')
+let masculino = document.getElementById('iop_masc')
+let feminino = document.getElementById('iop_femin')
+let campo_idade = document.getElementById('pessoa_idade')
 const botao_verificar = document.getElementById('verifica_idade')
 
 let date_now = new Date()
 let year_now = date_now.getFullYear()
 
-botao_verificar.addEventListener('click', function(){
-    console.log(ano_input)
-})
+let idade = year_now - ano_input
 
+botao_verificar.addEventListener('click', function(){
+    campo_idade.innerText = `A pessoa que nasceu em ${ano_input} tem ${idade} anos`
+    
+})
