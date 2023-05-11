db45fef48cae5722220bda1a29ab988533c8e998
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2023-05-10 22:45:08 -0300

    .

--- script.js
+++ script.js
@@ -9,28 +9,33 @@ const botao_verificar = document.getElementById('verifica_idade')
 let date_now = new Date()
 let year_now = date_now.getFullYear()
 
-const imagens = [
-    'imagens/idoso-1.jpg'
+const imagem_criança = [
+    box_imagem.setAttribute('src', 'imagens/garoto-1.jpg'),
+    box_imagem.setAttribute('src', 'imagens/garoto-2.jpg')
 ]
 
-const mudaImagem = () => {
-    console.log()
+function mudaImagem(idade){
+    if(idade > 1 && idade < 10){
+        let imagemAleatória = (Math.random() * imagem_criança.length)
+        return imagem_criança[imagemAleatória]
+    }
 }
 
 const mudaTexto = () => {
     const idade = year_now - ano_input.value
 
-    if(ano_input.value > year_now){
+    mudaImagem(idade)
+
+    if(ano_input.value > year_now || idade < 0){
+        campo_idade.innerHTML = `...`
         window.alert('Ano inválido')
     } 
     
-    if (masculino.checked == true){
+    if(masculino.checked == true){
         campo_idade.innerHTML = `A pessoa que nasceu em ${ano_input.value} é um homem de ${idade} anos`
     } else {
         campo_idade.innerHTML = `A pessoa que nasceu em ${ano_input.value} é um é uma mulher de ${idade} anos`
     }
-
-    box_imagem.setAtributte('src', mudaImagem)
 }
 
 botao_verificar.addEventListener('click', mudaTexto)
